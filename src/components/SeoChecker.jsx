23e7ecbd0c9bafdf1fb07c0d6b3f45fe0187d21b
sohaibkhan1125 <alihassankhan1192@gmail.com>
2023-12-11 08:40:31 +0500

    new

--- src/components/SeoChecker.jsx
+++ src/components/SeoChecker.jsx
@@ -18,7 +18,7 @@ function SeoChecker() {
       setIsLoading(true); 
 
       const startTime = performance.now();
-      const response = await axios.post('http://localhost:3001/analyze', { url });
+      const response = await axios.post('https://marvelous-valkyrie-f54533.netlify.app/analyze', { url });
       const endTime = performance.now();
 
       setSeoReport(response.data);
